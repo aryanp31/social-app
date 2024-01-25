9b1ae21d467345f3878df807f78d0d97b68c141b
Aryan Patel <aryanpatel31@yahoo.com>
2024-01-25 03:27:24 -0600

    Theme/Styling setup completed

---
+++ client/src/components/FlexBetween.jsx
@@ -0,0 +1,10 @@
+import { Box } from '@mui/material';
+import { styled } from '@mui/system';
+
+const FlexBetween = styled(box)({
+  display: "flex",
+  justifyContent: "spaceBetween",
+  alignItems: "center",
+});
+
+export default FlexBetween;
